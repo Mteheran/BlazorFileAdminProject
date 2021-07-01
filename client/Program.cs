fd347d8d08580a5915996d303d956c4b44b2076d
Miguel Teheran <miguelteheran@gmail.com>
2021-06-30 21:12:32 -0500

    last changes in Add form

--- client/Program.cs
+++ client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.Modal;
 
 namespace client
 {
@@ -18,6 +19,9 @@ namespace client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            builder.Services.AddBlazoredModal();
+
 
             await builder.Build().RunAsync();
         }
